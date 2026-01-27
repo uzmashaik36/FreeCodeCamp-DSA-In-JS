c3198cbdab5a789bae95a6494465c2935f6bf706
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2026-01-27 15:07:54 +0530

    Add files via upload

---
+++ Declare JavaScript Variables/index.js
@@ -0,0 +1,7 @@
+//Declaring Variables in JS
+
+//JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+
+// 1.Use the var keyword to create a variable called myName.
+
+var myName;
