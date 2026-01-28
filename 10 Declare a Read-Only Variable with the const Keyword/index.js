50d8e9103e3b1621552cd0c7ef29ab277c12b6e5
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2026-01-29 01:23:51 +0530

    Add files via upload

---
+++ 10 Declare a Read-Only Variable with the const Keyword/index.js
@@ -0,0 +1,24 @@
+//Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+/*   
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+*/
+
+/*
+
+1. var should not exist in your code.
+2. You should change fCC to all uppercase.
+3. FCC should be a constant variable declared with const.
+4. The string assigned to FCC should not be changed.
+5. fact should be declared with let.
+6. console.log should be changed to print the FCC and fact variables
+
+*/
+
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
