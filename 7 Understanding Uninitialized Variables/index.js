3658cde9dcef6d439f7822ea30b34c5e29fe3e5c
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2026-01-28 23:21:52 +0530

    Add files via upload

---
+++ 7 Understanding Uninitialized Variables/index.js
@@ -0,0 +1,12 @@
+//When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.
+
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
