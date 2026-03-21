9171708482d617d536616aa2afb9212467baac76
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2026-03-21 23:58:58 +0530

    Add files via upload

---
+++ 15 Increment a Number with JavaScript/index.js
@@ -0,0 +1,24 @@
+/*
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+
+
+
+instructions...
+1. myVar should equal 88.
+2. You should not use the assignment operator.
+3. You should use the ++ operator.
+4. You should not change code above the specified comment.
+*/
+
+
+//Change the code to use the ++ operator on myVar.
+
+//Answer
+
+let myVar = 87;
+
+ myVar++;
+ console.log(myVar);
